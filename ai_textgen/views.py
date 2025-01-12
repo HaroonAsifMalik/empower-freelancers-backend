6ddc6becd5d84cf7ac4e7348d7b3e59f0055eeeb
Haroon <haroonasifmalik123@gmail.com>
2025-01-12 14:53:58 +0500

    feat(): add generate text endpoint

--- ai_textgen/views.py
+++ ai_textgen/views.py
@@ -11,51 +11,46 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
-# @csrf_exempt
-# def interact_with_ollama(request):
-#     if request.method == "POST":
-#         try:
-            
-#             user_prompt =  WebScrapingJob.objects.first().description
-#             if not user_prompt:
-#                 return JsonResponse({"error": "Prompt is required"}, status=400)
 
-#             OLLAMA_API_URL = "http://localhost:11434/api/generate"
-#             headers = {"Content-Type": "application/json"}
-#             payload = {"model": "llama2:7b", "prompt": user_prompt, "max_token": 20}
-#             response = requests.post(OLLAMA_API_URL, headers=headers, json=payload)
-
-#             if response.status_code == 200:
-#                 results = []
-#                 for line in response.text.strip().split("\n"):
-#                     try:
-#                         data = json.loads(line)
-#                         results.append(data["response"])
-
-#                     except json.JSONDecodeError as e:
-#                         return JsonResponse(
-#                             {"error": "Malformed response", "details": str(e)},
-#                             status=500,
-#                         )
-#                 response_string = "".join(results)
-
-#                 return JsonResponse({"results": response_string}, safe=False)
-#             else:
-#                 return JsonResponse(
-#                     {
-#                         "error": "Error communicating with Ollama",
-#                         "details": response.text,
-#                     },
-#                     status=response.status_code,
-#                 )
-
-#         except Exception as e:
-#             return JsonResponse(
-#                 {"error": "An error occurred", "details": str(e)}, status=500
-#             )
-#     else:
-#         return JsonResponse({"error": "Invalid request method"}, status=405)
-
+@csrf_exempt
+def interact_with_ollama(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            user_prompt = data.get("prompt", "")
+            if not user_prompt:
+                return JsonResponse({"error": "Prompt is required"}, status=400)
+            OLLAMA_API_URL = "http://localhost:11434/api/generate"
+            headers = {"Content-Type": "application/json"}
+            payload = {"model": "llama2:7b", "prompt": user_prompt, "max_token": 20}
+            response = requests.post(OLLAMA_API_URL, headers=headers, json=payload)
+            if response.status_code == 200:
+                results = []
+                for line in response.text.strip().split("\n"):
+                    try:
+                        data = json.loads(line)
+                        results.append(data["response"])
+                    except json.JSONDecodeError as e:
+                        return JsonResponse(
+                            {"error": "Malformed response", "details": str(e)},
+                            status=500,
+                        )
+                response_string = "".join(results)
+                return JsonResponse({"results": response_string}, safe=False)
+            else:
+                return JsonResponse(
+                    {
+                        "error": "Error communicating with Ollama",
+                        "details": response.text,
+                    },
+                    status=response.status_code,
+                )
+        except Exception as e:
+            return JsonResponse(
+                {"error": "An error occurred", "details": str(e)}, status=500
+            )
+    else:
+        return JsonResponse({"error": "Invalid request method"}, status=405)
 
 
 class AIProposalResponseListView(APIView):
